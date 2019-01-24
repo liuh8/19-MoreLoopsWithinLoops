8473a33c83b0fc7910db791e98ee0f59bdba0016
liuh8 <liuh8@rose-hulman.edu>
2019-01-23 21:50:43 -0500

    more loops

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -4,8 +4,8 @@ in the context of PRINTING on the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Hongyu Liu.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -56,7 +56,7 @@ def triangle_right_justified(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # HINT: Do the following problem FIRST, then convert x's to spaces:
@@ -75,6 +75,12 @@ def triangle_right_justified(r):
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
 
+    for j in range(r):
+        for k in range(r - j -1):
+            print(' ', end='')
+        for i in range(j + 1):
+            print(i + 1, end='')
+        print()
 
 def run_test_triangle_upside_down():
     """ Tests the    triangle_upside_down    function. """
@@ -110,7 +116,7 @@ def triangle_upside_down(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -118,6 +124,12 @@ def triangle_upside_down(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
+    for j in range(r):
+        for k in range(j):
+            print(' ', end='')
+        for i in range(r - j):
+            print(i + 1, end='')
+        print()
 
 
 def run_test_vee():
@@ -161,7 +173,7 @@ def vee(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # done: 4. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -169,6 +181,15 @@ def vee(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
+    for j in range(r):
+        for k in range(j):
+            print(' ', end='')
+        for i in range(r - j):
+            print(i + 1, end='')
+        print('-', end='')
+        for i in range(r-j, 0, -1):
+            print(i, end='')
+        print()
 
 
 def run_test_numbers_constant_forward():
